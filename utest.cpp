27f991ea90aa513090c57dd170d29d9e5340632c
yukipyon <wolfvbear@gmail.com>
2015-02-17 18:13:00 +0000

    Added botan rng code to unit test.

--- utest.cpp
+++ utest.cpp
@@ -1,10 +1,16 @@
 #include "CppUTest/CommandLineTestRunner.h"
+#include <botan/auto_rng.h>
+#include <botan/init.h>
+#include <iostream>
 
 TEST_GROUP(first)
 {};
 
 TEST(first, test)
 {
+    Botan::LibraryInitializer init;
+    Botan::AutoSeeded_RNG rng;
+    std::cout << "hello world" << rng.next_byte() << std::endl;
     FAIL("Start here");
 }
 
