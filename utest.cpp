651f0d8cdba2d2fe16cbcd582e8d7a76aa06efbc
yukipyon <wolfvbear@gmail.com>
2015-02-19 18:10:09 +0000

    Fake rng.

--- utest.cpp
+++ utest.cpp
@@ -1,7 +1,22 @@
 #include "CppUTest/CommandLineTestRunner.h"
 #include <botan/auto_rng.h>
+#include <botan/rng.h>
 #include <botan/init.h>
 #include <iostream>
+
+namespace Botan {
+    struct fake_rng : public RandomNumberGenerator {
+        void randomize(byte[], size_t) {  }
+        void clear() {}
+        std::string name() const { return "Null_RNG"; }
+
+        byte next_byte() { return 6; };
+        void reseed(size_t) {}
+        bool is_seeded() const { return false; }
+        void add_entropy(const byte[], size_t) {}
+        void add_entropy_source(EntropySource* es) { delete es; }
+    };
+}
 
 TEST_GROUP(botan)
 {
