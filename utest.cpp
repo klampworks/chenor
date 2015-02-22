7e20ff15aaaadd8296043f95a9e566f0c75e97c6
yukipyon <wolfvbear@gmail.com>
2015-02-22 18:19:21 +0000

    Removed dead code.

--- utest.cpp
+++ utest.cpp
@@ -7,25 +7,6 @@
 #include <botan/pkcs8.h>
 #include "botan/Botan-1.10.5/checks/common.h"
 
-namespace Botan {
-    struct fake_rng : public RandomNumberGenerator {
-        void randomize(byte bytes[], size_t len) { 
-            for (size_t i = 0; i < len; ++i)
-                bytes[i] = 6;
-        }
-
-        byte random() { return 6; }
-        void clear() {}
-        std::string name() const { return "fake_rng"; }
-
-        byte next_byte() { return 6; };
-        void reseed(size_t) {}
-        bool is_seeded() const { return true; }
-        void add_entropy(const byte[], size_t) {}
-        void add_entropy_source(EntropySource* es) { delete es; }
-    };
-}
-
 #define MK_FAKE_RNG_INC(name) MK_FAKE_RNG(name, i)
 #define MK_FAKE_RNG_SIX(name) MK_FAKE_RNG(name, 6)
 
@@ -63,7 +44,7 @@ TEST(botan, rng_gives_different_numbers)
 TEST(botan, fake_rng_gives_fixed_number)
 {
     Botan::LibraryInitializer init;
-    Botan::fake_rng rng;
+    MK_FAKE_RNG_SIX(rng);
 
     CHECK_EQUAL(rng.next_byte(), rng.next_byte());
     CHECK_EQUAL(6, rng.next_byte());
@@ -72,22 +53,10 @@ TEST(botan, fake_rng_gives_fixed_number)
 TEST(botan, create_key)
 {
     Botan::LibraryInitializer init;
-    //Botan::fake_rng rng;
+    MK_FAKE_RNG_INC(rng);
 
-    Botan::SecureVector<byte> in;
-    for (int i = 0; i < 10000; ++i)
-        in.push_back(i);
-
-    Fixed_Output_RNG rng(in);
-    //Botan::AutoSeeded_RNG rng;
-
-    //terminate called after throwing an instance of 'Botan::Internal_Error'
-    //  what():  Internal error: Assertion self_test_signature(encoded, plain_sig) failed (PK_Signer consistency check failed) in Botan::SecureVector<unsigned char> Botan::PK_Signer::signature(Botan::RandomNumberGenerator&) @src/pubkey/pubkey.cpp:218
-    //  Aborted
     auto private_key = Botan::RSA_PrivateKey(rng, 1024);
     //auto public_key = Botan::X509::PEM_encode(private_key);
-
-
 }
 
 int main(int argc, char **argv)
