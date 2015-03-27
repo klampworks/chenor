9ac5752e728f33821b311d5780d4ef13e4f7b491
yukipyon <wolfvbear@gmail.com>
2015-03-27 18:16:15 +0000

    Refactor.

--- utest.cpp
+++ utest.cpp
@@ -405,30 +405,25 @@ TEST(botan, pipe_base64_encode_to_file_decode_from_file)
 {
     Botan::LibraryInitializer init;
 
-    const std::string fn_out("pipe_base64_encode_to_file.txt");
-    const std::string fn_in("pipe_base64_decode_from_file.txt");
+    const std::string fn_enc("pipe_base64_encode_to_file.txt");
+    const std::string fn_dec("pipe_base64_decode_from_file.txt");
 
-    std::ofstream ofs(fn_out, std::ios::binary);
+    std::ofstream ofs(fn_enc, std::ios::binary);
     const std::string in("hello");
 
     Botan::Pipe pipe(new Botan::Base64_Encoder, new Botan::DataSink_Stream(ofs));
-
-    pipe.start_msg();
-    pipe.write(in);
-    pipe.end_msg();
-
+    pipe.process_msg(in);
 
     ofs.close();
-    ofs.open(fn_in, std::ios::binary);
+    ofs.open(fn_dec, std::ios::binary);
+    Botan::DataSource_Stream dss(fn_enc);
 
     Botan::Pipe pipe2(new Botan::Base64_Decoder, new Botan::DataSink_Stream(ofs));
-
-    Botan::DataSource_Stream dss(fn_out);
     pipe2.process_msg(dss);
 
     ofs.close();
 
-    const std::string out = read_file(fn_in);
+    const std::string out = read_file(fn_dec);
     CHECK_EQUAL(out, in);
 }
 
