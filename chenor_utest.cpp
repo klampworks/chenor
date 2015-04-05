605929584cefeb4714edaa34bf5e6a75dffe9331
yukipyon <wolfvbear@gmail.com>
2015-04-05 18:10:53 +0000

    Mock example.

--- chenor_utest.cpp
+++ chenor_utest.cpp
@@ -1,12 +1,37 @@
 #include "CppUTest/CommandLineTestRunner.h"
+#include "CppUTestExt/MockSupport.h"
+#include "chenor.hpp"
+//#include <unistd.h>
 
 TEST_GROUP(chenor_write)
 {
+    void teardown()
+    {
+        mock().clear();
+    }
 };
+
+
+ssize_t write(int fd, void *buf, size_t count)
+{
+    return mock().actualCall("write")
+        .withParameter("fd", fd)
+        .withOutputParameter("buf", buf)
+        .withParameter("count", count)
+        .returnIntValue();
+}
 
 TEST(chenor_write, start_here)
 {
-    FAIL("start_here");
+    char buf[] = "hello world";
+    mock().expectOneCall("write")
+        .withParameter("fd", 1)
+        .withOutputParameterReturning("buf", buf, sizeof buf)
+        .withParameter("count", sizeof buf)
+        .andReturnValue(666);
+
+    CHECK_EQUAL(666, write(1, buf, sizeof buf));
+    mock().checkExpectations();
 }
 
 int main(int argc, char **argv)
