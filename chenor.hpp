9dab17f3ca4f5fd95a7c792b0d11bb31732a6ed1
yukipyon <wolfvbear@gmail.com>
2015-07-11 18:04:42 +0000

    Fix some visability issues in the header.

--- chenor.hpp
+++ chenor.hpp
@@ -10,21 +10,21 @@ namespace Botan {
     class PK_Encryptor_EME;
 }
 
-//namespace chenor {
+class chenor {
 
-struct chenor {
+    public:
     ssize_t write(int fd, const void *buf, size_t count);
     std::string decrypt(const std::vector<char> &in, Botan::RSA_PrivateKey *pk);
-
-    std::shared_ptr<Botan::RSA_PublicKey> public_key;
-    std::shared_ptr<Botan::AutoSeeded_RNG> rng;
-    std::shared_ptr<Botan::PK_Encryptor_EME> pke;
     static Botan::RSA_PrivateKey* gen_key();
-    std::shared_ptr<Botan::AutoSeeded_RNG> get_rng();
-    std::string eme;
-
     //Since we are forward declaring these key types the compiler does not know
     // that one inherits the other, hence we will overload the function.
     chenor(Botan::RSA_PrivateKey *pk);
     chenor(Botan::RSA_PublicKey *pk=nullptr);
+
+    private:
+    std::shared_ptr<Botan::RSA_PublicKey> public_key;
+    std::shared_ptr<Botan::AutoSeeded_RNG> rng;
+    std::shared_ptr<Botan::PK_Encryptor_EME> pke;
+    std::shared_ptr<Botan::AutoSeeded_RNG> get_rng();
+    std::string eme;
 };
